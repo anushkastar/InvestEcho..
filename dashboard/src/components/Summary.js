1ceb293e47296c352c961b51dc9fd00216f12785
Anushka Tripathi <162957357+anushkastar@users.noreply.github.com>
2025-05-30 23:59:28 +0530

    Add files via upload

---
+++ dashboard/src/components/Summary.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+
+const Summary=()=> {
+    return (
+        <>
+        <div className='username'>
+            <h6>Hi, User!</h6>
+            <hr className='divider'></hr>
+        </div>
+        
+
+        <div className='section'>
+            <span>
+                <p>Equity</p>
+            </span>
+        
+
+        <div className='data'>
+            <div className='first'>
+                <h3>3.74k</h3>
+                <p>Margin Available</p>
+            </div>
+
+            <hr />
+
+            <div className='second'>
+                <p>
+                    Margins used <span>0</span>{" "}
+                </p>
+                <p>
+                    Opening balance <span>3.74k</span>{" "}
+                </p>
+                
+            </div>
+        </div>
+        <hr className='divider'></hr>
+        </div>
+
+        <div className='section'>
+            <p>Holdings (13)</p>
+        </div>
+
+        <div className='data'>
+            <div className='first'>
+                <h3 className='profit'>
+                    1.55k <small>+5.20%</small>{" "}
+                </h3>
+                <p>P&L</p>
+            </div>
+            <hr />
+
+            <div className='second'>
+                <p>
+                    Current Value <span>31.43k</span>{" "}
+                </p>
+                <p>
+                    Investment <span>29.88k</span>{" "}
+                </p>
+            </div>
+        </div>
+        <hr className='divider' />
+        <div />
+        
+        </>
+    );
+};
+
+export default Summary;
